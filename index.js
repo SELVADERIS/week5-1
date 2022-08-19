e7f25f2da53ba343150b3a3a36fe16e74e4c6be7
SELVADERIS <selva.deris.1395@gmail.com>
2022-08-19 17:24:04 +0300

    solved

---
+++ index.js
@@ -0,0 +1,17 @@
+
+document.getElementById("sms").innerHTML = "Hey I’m red!";
+document.getElementById("sms").style.color = "red";
+document.getElementById("h3").innerHTML = "I’m a blue h3!";
+document.getElementById("h3").style.color = "blue";
+document.getElementById("border").style.cssText = 'border:black; background:pink'; 
+const h = document.createElement("h1");
+h.innerHTML = "I’m in a div";
+document.getElementById("border").appendChild(h);
+const paragraph = document.createElement("p");
+paragraph.innerHTML = "ME TOO!";
+document.getElementById("border").appendChild(paragraph);
+
+
+
+
+
